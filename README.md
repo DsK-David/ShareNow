f50e983bdb014582be1ec003d1006d231589a90e
DsK-David <fbi.gov2015@gmail.com>
2023-06-23 09:32:59 -0100

    add files

---
+++ README.md
@@ -0,0 +1,52 @@
+# ShareNow - Compartilhamento de Arquivos
+
+O ShareNow é um projeto de compartilhamento de arquivos simples e fácil de usar. Com esta aplicação web, você pode fazer upload de arquivos para compartilhar com outras pessoas, gerar links para download e visualizar os arquivos compartilhados.
+
+## Recursos
+
+- Upload de arquivos para compartilhamento
+- Geração de links para download dos arquivos compartilhados
+- Lista de arquivos compartilhados com ícones e estilos atraentes
+
+## Tecnologias utilizadas
+
+- Node.js
+- Express.js
+- Multer (para o upload de arquivos)
+- HTML
+- CSS
+- Font Awesome (para ícones)
+
+## Como executar o projeto
+
+1. Certifique-se de ter o Node.js instalado em sua máquina.
+2. Faça o download ou clone este repositório.
+3. Navegue até o diretório do projeto no terminal.
+4. Execute o comando `npm install` para instalar as dependências.
+5. Execute o comando `npm share` para iniciar o servidor.
+6. Abra seu navegador e acesse `http://localhost:3000`.
+
+## Como usar o ShareNow
+3. Faça upload de um arquivo:
+   - Na página principal após fazer login, clique em "Escolha um arquivo".
+   - Selecione um arquivo em seu computador.
+   - Clique em "Enviar" para fazer o upload do arquivo.
+4. Compartilhe o arquivo:
+   - Após o upload bem-sucedido, um link para download do arquivo será gerado.
+   - Copie o link e compartilhe-o com outras pessoas.
+5. Visualize os arquivos compartilhados:
+   - Na página principal após fazer login, você verá uma lista de arquivos compartilhados.
+   - Clique no nome do arquivo para fazer o download.
+
+## Observações
+
+- Os arquivos compartilhados são armazenados no diretório "uploads" do servidor. Certifique-se de que o diretório tenha as permissões adequadas de leitura e gravação.
+- Este projeto é fornecido como uma base inicial para desenvolvimento. Sinta-se à vontade para expandir e aprimorar de acordo com suas necessidades.
+
+## Licença
+
+Este projeto está licenciado sob a [MIT License](https://opensource.org/licenses/MIT).
+
+---
+
+Esperamos que o ShareNow facilite o compartilhamento de arquivos e contribua para suas necessidades de compartilhamento online. Sinta-se à vontade para contribuir, enviar feedback ou relatar problemas. Aproveite!
---
+++ src/index.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+
+// Configuração do Multer para o armazenamento de arquivos
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, 'uploads'));
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+});
+
+const upload = multer({ storage });
+
+app.use(express.static('public'));
+
+// Rota principal para renderizar o formulário de upload
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Rota para o upload de arquivos
+app.post('/upload', upload.single('file'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).send('Nenhum arquivo foi enviado.');
+  }
+
+  const sharedLink = `${req.protocol}://${req.get('host')}/shared/${req.file.filename}`;
+  res.send(`Arquivo compartilhado com sucesso! Envie o link a seguir ao seu amigo: ${sharedLink}`);
+});
+
+// Rota para listar os arquivos compartilhados
+app.get('/shared', (req, res) => {
+  const sharedDir = path.join(__dirname, 'uploads');
+
+  fs.readdir(sharedDir, (err, files) => {
+    if (err) {
+      console.error('Erro ao ler a pasta compartilhada:', err);
+      return res.status(500).send('Erro ao acessar a pasta compartilhada.');
+    }
+
+    const fileLinks = files.map(file => ({
+      name: file,
+      link: `${req.protocol}://${req.get('host')}/shared/${file}`
+    }));
+
+    const html = `
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+    <h2><i class="fas fa-file-alt"></i> Arquivos compartilhados</h2>
+    <ul class="file-list">
+      ${fileLinks.map(file => `<li><a style="color:blue"; href="${file.link}"><i class="fas fa-file"></i> ${file.name}</a></li>`).join('')}
+    </ul>
+  `;
+  
+  res.send(html);
+  });
+});
+
+// Rota para fazer o download de um arquivo compartilhado
+app.get('/shared/:filename', (req, res) => {
+  const filename = req.params.filename;
+  const filePath = path.join(__dirname, 'uploads', filename);
+
+  res.download(filePath, filename, (err) => {
+    if (err) {
+      console.error('Erro ao fazer o download do arquivo:', err);
+      res.status(500).send('Erro ao fazer o download do arquivo.');
+    }
+  });
+});
+
+// Inicia o servidor
+app.listen(3000, () => {
+  console.log('Servidor iniciado na porta 3000');
+});
